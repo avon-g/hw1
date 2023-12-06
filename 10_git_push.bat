c207b3b400fe5f59a9b8503f19a5c8908171556e
avon-g <y992@ya.ru>
2023-12-06 10:08:59 +0300

    start

---
+++ 10_git_push.bat
@@ -0,0 +1,10 @@
+git add *.bat
+git add hw1.iml
+git add log4j-api-2.12.4.jar
+git add log4j-core-2.12.4.jar
+git add Main.java
+git add log4j2.properties
+git commit -m "start"
+git branch -M main
+git remote add origin https://github.com/avon-g/hw1.git
+git push -u origin main
---
+++ Main.java
@@ -0,0 +1,26 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
+//import org.apache.logging.log4j;
+//import org.apache.log4j.PropertyConfigurator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
+
+public class Main {
+
+    final static Logger logger = LogManager.getLogger(Main.class);;
+
+    public static void main(String[] args) {
+
+        logger.info("Start HW#1");
+    }
+}
+
+
+
