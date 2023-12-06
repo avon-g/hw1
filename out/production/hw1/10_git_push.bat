8fa1c24164a2f99a5d4e73121140e53450a789a2
avon-g <y992@ya.ru>
2023-12-07 00:37:33 +0300

    start2

--- out/production/hw1/10_git_push.bat
+++ out/production/hw1/10_git_push.bat
@@ -4,7 +4,7 @@ git add log4j-api-2.12.4.jar
 git add log4j-core-2.12.4.jar
 git add Main.java
 git add log4j2.properties
-git commit -m "start2"
+git commit -m "start"
 git branch -M main
 git remote add origin https://github.com/avon-g/hw1.git
 git push -u origin main
