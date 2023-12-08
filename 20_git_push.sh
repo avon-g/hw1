21c56316432d9cbe32e858838de5b82810b1b939
avon-g <y992@ya.ru>
2023-12-08 07:53:48 +0300

    hosting1

--- 20_git_push.sh
+++ 20_git_push.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git add *.bat
 git add *.sh
 git add hw1.iml
@@ -12,9 +13,9 @@ git add manifest.txt
 git add Sales.java
 git add Seller.java
 git add Sellers.java
-git add SendMail.java 
+git add SendMail.java
 
-git commit -m "compiled locally"
+git commit -m "hosting1"
 git branch -M main
 git remote add origin https://github.com/avon-g/hw1.git
 git push -u origin main
